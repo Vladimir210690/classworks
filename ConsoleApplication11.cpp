bd5a3e96cf4eff0783dad96dc295df5fbe700786
Vladimir <146058049+Vladimir210690@users.noreply.github.com>
2023-11-13 19:44:40 +0300

    Tablica umnogenia

---
+++ ConsoleApplication11.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int col = 10;
+int row = 10;
+
+int main()
+{
+	for (int i = 0; i < col; i++)
+	{
+		for (int j = 0; j < row; j++)
+		{
+
+			if (i * j == 0) {
+				continue;
+			}
+			if (i * j < 10)
+			{
+				cout << "  " << i * j << " ";
+			}
+			else
+				cout << " " << i * j << " ";
+		
+		}
+		cout << endl;
+	}
+	cout << endl;
+	return 0;
+}
