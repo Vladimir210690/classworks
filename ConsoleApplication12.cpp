36b816249f21ee308ecd604284a1b690a803bb15
Vladimir <146058049+Vladimir210690@users.noreply.github.com>
2023-11-15 20:31:11 +0300

    Задание 3
    Недоделано, надо реверсировать цифры или преобразовать их в число

---
+++ ConsoleApplication12.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int number = 0;
+	int a = 0;
+	int b = 0;
+	cin >> number;
+	int col = 0;
+	int c = 0;
+	c = number;
+	int v = 0;
+	int x = 0;
+	
+	for (;;) {
+		a = number % 10;
+		b = number / 10;
+		number = b;
+		col++;
+		if (b == 0)
+			break;
+	
+	}
+
+	for (int i = 0; i < col; i++) {
+		a = c % 10;
+		b = c / 10;
+		c = b;
+		
+		if (a == 3 || a == 6) {
+			continue;
+		}
+		cout << a;
+	}
+	
+	return 0;
+}
