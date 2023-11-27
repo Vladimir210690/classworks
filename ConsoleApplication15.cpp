fa1cf345dce0f3934f2d2859eb54d179c8a65d7b
Vladimir <146058049+Vladimir210690@users.noreply.github.com>
2023-11-27 19:19:04 +0300

    sizeof
    sizeof massive

---
+++ ConsoleApplication15.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	int arr[10]{ 25,36,98,2,45,1,65,97,47,33 };
+
+	int real = sizeof(arr) / sizeof(arr[0]);
+	cout << real << endl << endl;
+	for (int i = 0; i < real; i++) {
+		cout << arr[i] << endl;
+	}
+	cout << endl;
+	return 0;
+}
+
