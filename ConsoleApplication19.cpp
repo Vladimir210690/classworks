e07202b0992e80a7257cc4601f5652d0a59267bd
Vladimir <146058049+Vladimir210690@users.noreply.github.com>
2023-12-04 19:42:21 +0300

    Замена значений массива друг с другом
    1 Загрузил массив с помощью рандома
    2 Нашел максимальное значение
    3 Поменял максимальное значение с последним элементом

---
+++ ConsoleApplication19.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main() {
+	srand(time(NULL));
+	setlocale(LC_ALL, "RUS");
+	int max = 0;
+	int arr[10]{};
+
+	for (int i = 0; i < 10; i++) {
+		arr[i] = rand() % 89 + 10;
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	max = arr[0];
+	int number = 0;
+	int maxindex = 0;
+	for (int i = 1; i < 10; i++) {
+		if (arr[i] > max) {
+			max = arr[i];
+			maxindex = i;
+			
+		}
+		 number = arr[9];
+	}
+	cout << "Maxindex = " << maxindex << endl;
+	cout << "Max = " << max << endl;
+
+	arr[9] = max;
+	arr[maxindex] = number;
+
+	for (int i = 0; i < 10; i++) {
+		cout << arr[i] << " ";
+	}
+
+
+
+
+	return 0;
+}
