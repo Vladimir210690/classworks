a2999ebafc0f927eda102bc47317a1d1cbb7036c
Vladimir <146058049+Vladimir210690@users.noreply.github.com>
2023-12-13 19:32:33 +0300

    Урок возвращения значения функцией

---
+++ УРОК13ДЕКАБРЯ.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int Min(int a, int b) {
+    if (a <= b) {
+        return a;
+    }
+    else  {
+        return b;
+    }
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int c = 0, d = 0;
+    cout << "Введите первое число: ";
+    cin >> c;
+    cout << "Введите второе число: ";
+    cin >> d;
+
+    cout << Min(c, d);
+
+    return 0;
+}
