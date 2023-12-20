e2ecb0a40ee3bf64ba87e6560c50afa5b19a4fe3
Vladimir <146058049+Vladimir210690@users.noreply.github.com>
2023-12-20 19:24:28 +0300

    Шаблонная функция
    Бонусом изучил ключевое слово: Авто

--- Source.cpp
+++ Source.cpp
@@ -1,50 +1,30 @@
 #include <iostream>
-#include <ctime>
 
 using namespace std;
 
-void Revers(int arr[], int n) {
-	for (int i = 0, j = n - 1; i < j; i++, j--) {
-		swap(arr[i], arr[j]);
-	}
+template <typename T1, typename T2>
+
+T1 Foo(T1 a, T2 b) {
+
+
+	return a + b;
 }
 
-void Bubble(int bubble[], int n) {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n - 1; j++) {
-			if (bubble[j] < bubble[j + 1]) {
-				swap(bubble[j], bubble[j + 1]);
-			}
-		}
-	}
+auto Foo2(int a, double b) {
 
+	return a + b;
 }
 
+short main() {
+	setlocale(LC_ALL, "RU");
+	long long number = 123456789123456789;
 
-int main() {
-	setlocale(LC_ALL, "Russian");
-	srand(time(NULL));
-
-	const int SIZE = 10;
-
-	int mass[SIZE]{};
-
-	for (int i = 0; i < SIZE; i++) {
-		mass[i] = rand() % 100;
-		cout << mass[i] << " ";
-	}
-	cout << endl << endl;
-	Bubble(mass, SIZE);
-
-	for (int i = 0; i < SIZE; i++) {
-		cout << mass[i] << " ";
-	}
-	cout << endl << endl;
-	Revers(mass, SIZE);
-
-	for (int i = 0; i < SIZE; i++) {
-		cout << mass[i] << " ";
-	}
+	cout << sizeof(long long) << endl;
+	cout << number << endl;
+	cout << "Привет мир" << endl;
+	cout << Foo(3.5,5) << endl;
+	cout << Foo2(3, 5.9) << endl;
 
 	return 0;
 }
+
