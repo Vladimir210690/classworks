e44f01d6f453533cda82ee0c091a88874a7dc3af
Vladimir <146058049+Vladimir210690@users.noreply.github.com>
2024-01-11 20:09:58 +0300

    Динамический массив
    От скуки быстренько набросал создание и инициализацию динамического массива, с последующим удалением и обнулением указателя

--- Source.cpp
+++ Source.cpp
@@ -1,26 +1,13 @@
 #include <iostream>
-#include <string>
 
-using namespace std;
+void main() {
+	std::cout << "Hello Word";
 
-int main() {
-	const char str[] = "Hello World";
-	cout << strlen(str) << "\t";
-	const char* str2 = "Goodbuy";
-	cout << strlen(str2) << "\t";
-
-	string str3 = "motion";
-	cout << str3.length() << "\t";
-
-	char str4[100];
-	str4[0] = 'H';
-	str4[1] = '\0';
-	cout << str4 << "\t";
-
-	const char* str5 = "template";
-	const char* str6 = "template";
-
-	cout << strcmp(str5, str6);
-
-	return 0;
+	int *pn = new int[1000];
+	for (int i = 0; i < 1000; i++) {
+		*(pn + i) = 2;
+		std::cout << *pn << "\t";
+	}
+	delete[]pn;
+	pn = nullptr;
 }
