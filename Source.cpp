e3adfea74c90a812e59914169e45db0722fa8ab8
Vladimir <146058049+Vladimir210690@users.noreply.github.com>
2024-01-17 20:00:54 +0300

    getline
    teacher function string getline()

--- Source.cpp
+++ Source.cpp
@@ -1,33 +1,11 @@
 #include <iostream>
 #include <string>
-#include <ctime>
 
 using namespace std;
 
-struct People {
-	int age = 0;
-	string name;
-};
-People arr[3];
-
 int main() {
-	srand(time(NULL));
-	setlocale(LC_ALL, "Russian");
-
-	string names;
-
-	for (int i = 0; i < 3; i++) { // Заполняем структуру данными людей
-		cout << "Введите ваше имя: ";
-		cin >> arr[i].name;
-		cout << "Введите ваш возраст: ";
-		cin >> arr[i].age;
-		system("cls");
-	}
-	cout << endl << endl;
-
-	cout << "ID\t" << "name\t" << "age" << endl;
-	for (int i = 0; i < 3; i++) { // Выгружаем структуру с данными людей
-		cout << i + 1 << "\t" << arr[i].name << "\t" << arr[i].age << endl;
-	}
+	string name;
+	getline(cin, name);
+	cout << name;
 	return 0;
 }
