ace59f4fdba78ec99d35f1a9b78fdc1ae4304b6c
Vladimir <146058049+Vladimir210690@users.noreply.github.com>
2024-02-05 19:17:41 +0300

    Constr and Distr

--- FileName.cpp
+++ FileName.cpp
@@ -3,28 +3,44 @@
 using namespace std;
 
 class Cars {
+	
 private:
-	int number;
-	int weight;
+	int* number = 0;
+	int *year = 0;
+	string* name = 0;
 public:
-	void setNum(int num) {
-		
-		number = num;
+	void Motion() {
+		cout << "Start motion" << endl;
 	}
-	int getNum() {
-		cout << "Number car: " << number;
-		return number;
+	void Data() {
+		number = new int();
+		year = new int();
+		name = new string();
+	}
+	void Input() {
+		cout << "Input number, year, name: " << endl;
+		cin >> *number >> *year >> * name;
+	}
+	void Print() {
+		cout << *number << "\t" << *year << "\t" << *name << endl;
+	}
+	Cars() {
+		cout << "Конструктор сработал" << endl;
+	}
+	~Cars() {
+		delete number, year, name;
+		cout << "Деструктор сработал" << endl;
 	}
 };
 
-
-
 int main() {
-	Cars BMV;
-	int num = 0;
-	cin >> num;
-	BMV.setNum(num);
-	BMV.getNum();
-
+	setlocale(LC_ALL, "Russian");
+	Cars Moto;
+	Moto.Motion();
+	Moto.Data();
+	Moto.Input();
+	Moto.Motion();
+	Moto.Print();
+	
 	return 0;
 }
